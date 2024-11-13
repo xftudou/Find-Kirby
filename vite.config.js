a2928b8addd743839ea410f28eb806462291a083
rmalina <yang.yuhan3@northeastern.edu>
2024-11-12 17:54:18 -0800

    test render with SPA routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   publicDir: 'public',
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+    },
+  }
 })
